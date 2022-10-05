6efbf4e766131909686d3a26fe4e2066c7204c2f
ezraaillet <ezraaillet204@gmail.com>
2022-10-04 21:08:48 -0500

    initial setup

---
+++ src/Home.js
@@ -0,0 +1,13 @@
+import './Home.css'
+
+import React from 'react';
+
+export default function Home() {
+    return (
+        <>
+            <div className='site-header'>
+                <h1>FindMyPlates</h1>
+            </div>
+        </>
+    )
+}
