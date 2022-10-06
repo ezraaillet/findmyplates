3ee3a463c4efed9e1f96388e28e69835e7162cc0
ezraaillet <ezraaillet204@gmail.com>
2022-10-05 22:42:15 -0500

    details panel and styling

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 
+import Details from './details/Details';
 import Home from './Home';
 import Result from './result/Result.js';
 import Search from './input/Search';
@@ -10,7 +11,16 @@ function App() {
       <div className='app'>
         <div className='home'><Home /></div>
         <div className='search'><Search /></div>
-        <div className='result'><Result /></div>
+
+        <div className="result-group">
+          <div className='result'>
+            <Result />
+          </div>
+          <div className='details'>
+            <Details />
+          </div>
+        </div>
+
       </div>
     </>
   )
